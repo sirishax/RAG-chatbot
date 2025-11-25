5e2f3cc838b58041de57e73727b2b04a9090debd
sirishax <rsirisha551@gmail.com>
2025-11-25 14:54:41 +0530

    fix: Remove langchain-huggingface dependency, use community embeddings

--- backend/server.py
+++ backend/server.py
@@ -9,7 +9,7 @@ import shutil
 # Import from langchain
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv, find_dotenv
